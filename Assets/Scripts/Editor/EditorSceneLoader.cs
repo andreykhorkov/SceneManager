91d44d18cda7b5f70c591cd62fd0ed6f7208a80b
Andrey Khorkov <and.khorkov@turingsense.com>
2018-06-22 15:35:51 -0700

    начало.

---
+++ Assets/Scripts/Editor/EditorSceneLoader.cs
@@ -0,0 +1,53 @@
+﻿using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[InitializeOnLoad]
+[CustomEditor(typeof(Temp))]
+public class EditorSceneLoader : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        if (GUILayout.Button("suka", EditorStyles.miniButton))
+        {
+            EditorSceneManager.OpenScene("Assets/Scenes/1.unity", OpenSceneMode.Additive);
+        }
+    }
+
+    void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnLevelFinishedLoading;
+    }
+
+    void OnLevelFinishedLoading(Scene scene, LoadSceneMode mode)
+    {
+        Debug.Log("Level Loaded");
+        Debug.Log(scene.name);
+        Debug.Log(mode);
+    }
+}
+
+[InitializeOnLoad]
+public static class LatestScenes
+{
+    private static string currentScene;
+
+    static LatestScenes()
+    {
+        currentScene = EditorApplication.currentScene;
+        EditorApplication.hierarchyWindowChanged += hierarchyWindowChanged;
+    }
+
+    private static void hierarchyWindowChanged()
+    {
+        if (currentScene != EditorApplication.currentScene)
+        {
+            //a scene change has happened
+            Debug.Log("Last Scene: " + currentScene);
+            currentScene = EditorApplication.currentScene;
+        }
+    }
+}
